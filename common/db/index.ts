f1a8ad6f2168d5762c228b8a8ea0f6b670db664c
Ravi Lodhi <ravi.lodhi@hotwaxsystems.com>
2026-09-07 16:20:17 +0530

    feat(db): move useSeedData into common, with the db and status colour injected

--- common/db/index.ts
+++ common/db/index.ts
@@ -8,6 +8,7 @@ export * from "./projection";
 export * from "./baseDb";
 export * from "./dbClient";
 export * from "./useDb";
+export * from "./useSeedData";
 export * from "./useDbStatus";
 export * from "./sync/syncRegistry";
 export * from "./sync/workerFetch";
